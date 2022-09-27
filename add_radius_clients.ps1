65aa5b5f72073eac38fc858d7ba6e460c9131eda
JoeWetmore <josephwetmore@gmail.com>
2022-09-27 12:33:25 -0600

    Create add_radius_clients.ps1

---
+++ add_radius_clients.ps1
@@ -0,0 +1,19 @@
+ Import-Csv "c:\scipts\rad_client_list.csv" | Foreach-Object
+ {
+     $DeviceName = $_."Name"
+     $IPAddress = $_."IP"
+     $SecretPass = $_."SecretKey"
+     New-NpsRadiusClient -Address $IPAddress -Name $DeviceName -SharedSecret $SecretPass -VendorName "RADIUS Standard" -AuthAttributeRequired $True
+ }
+ 
+ 
+ 
+ # CSV sample
+ Name,IP,SecretKey
+ WAP114,10.10.255.82,ocmaradiusss
+ WAP115,10.10.255.83,ocmaradiusss
+ 
+ # REF https://learn.microsoft.com/en-us/answers/questions/358638/how-to-add-bulk-radius-client-in-nps-using-powersh.html
+ 
+ 
+ 
