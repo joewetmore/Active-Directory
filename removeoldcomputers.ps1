1718ed8022781a80c0b3c906004d8f7c4c37b844
JoeWetmore <josephwetmore@gmail.com>
2023-08-15 09:54:21 -0600

    Create removeoldcomputers.ps1

---
+++ removeoldcomputers.ps1
@@ -0,0 +1,11 @@
+# https://petri.com/find-remove-inactive-accounts-from-active-directory-powershell/
+
+get-adcomputer -filter * -properties passwordlastset | select name, passwordlastset | sort passwordlastset
+
+$date = (get-date).addyears(-1)
+or
+$date = (get-date).adddays(-16).
+
+get-adcomputer -filter {passwordlastset -lt $date} -properties passwordlastset | select name, passwordlastset | sort passwordlastset
+
+get-adcomputer -filter {passwordlastset -lt $date} -properties passwordlastset | remove-adobject -recursive -verbose -confirm:$false
